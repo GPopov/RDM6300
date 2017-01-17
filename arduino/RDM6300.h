50bf1fba65b904dc6e4b70dd2f3b599685699089
Georgi Popov <popovga@gmail.com>
2017-01-17 17:51:45 +0200

    Refactor for readability, remove _new flag

--- arduino/RDM6300.h
+++ arduino/RDM6300.h
@@ -2,26 +2,34 @@
   RFID RDM6300 data decoder library
   (c) Stephane Driussi 20150623
   Not for commercial use
-
-  Refer to rdm6300_decoder_wiring.jpg diagram for proper connection
-
 */
 
 #ifndef RDM6300_H
 #define RDM6300_H
 
-#include <RDM6300.h>
-
 class RDM6300 {
-    public:
-        bool decode(uint8_t input);
-        RDM6300(uint8_t Data[]);
-        uint8_t *_Payload; // used for read comparisons
-    private:
-        int _seq, _cnt;
-        bool _new;
-        uint8_t _temp, _checksum;
+public:
+    bool decode(uint8_t input);
+    
+    RDM6300(uint8_t Data[]);
+    uint8_t *_Payload; // used for read comparisons
+private:
+    static uint8_t ASCIIToByte(uint8_t input);
+    enum Operation
+    {
+        Operation_BeginRead,
+        Operation_ReadMSB,
+        Operation_ReadLSB,
+        Operation_ValidateChecksum,
+        Operation_EndRead
+    };
+private:
+    int _cnt;
+    Operation _currentOperation;
+    uint8_t _temp, _checksum;
 };
 
 #endif
+
+
 
