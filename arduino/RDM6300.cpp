b581bb19a244a115603748bc8beef3666ca08fc1
Georgi Popov <popovga@gmail.com>
2017-01-18 15:16:49 +0200

    Refactor the payload into a dedicated class

--- arduino/RDM6300.cpp
+++ arduino/RDM6300.cpp
@@ -1,68 +1,96 @@
-/*
-  RFID RDM6300 data decoder library
-  (c) Stephane Driussi 20150623
-  Not for commercial use
-*/
 #include "stdafx.h"
 #include "RDM6300.h"
-
-RDM6300::RDM6300(uint8_t Data[])
-    :_currentOperation(Operation_BeginRead)
-    , _cnt(0)
-    , _temp(0)
+namespace RFID
+{
+Payload::Payload(const std::initializer_list<uint8_t>& list)
+{
+    unsigned i = 0;
+    for (auto d : list)
     {
-        _Payload = Data;
+        m_Payload[i] = d;
+        ++i;
     }
+}
+
+bool Payload::operator==(const Payload& other) const
+{
+    if (m_Checksum != other.m_Checksum)
+    {
+        return false;
+    }
+
+    for (size_t i = 0; i < MANUFACTURER_SIZE; ++i)
+    {
+        if (m_Manufacturer[i] != other.m_Manufacturer[i])
+            return false;
+    }
+
+    for (size_t i = 0; i < ID_SIZE; ++i)
+    {
+        if (m_ID[i] != other.m_ID[i])
+            return false;
+    }
+    return true;
+}
+
+RDM6300::RDM6300(Payload& p)    
+    : m_Cnt(0)
+    , m_CurrentOperation(Operation_BeginRead)
+    , m_Checksum(0)
+    , m_Temp(0)
+    , m_Payload(p){
+
+}
 
 bool RDM6300::decode(uint8_t input) {
 
     if (input == 0x02)
     {
-        _cnt = 0;
-        _checksum = 0;        
-        _temp = 0;
-        _currentOperation = Operation_BeginRead;
+        m_Cnt = 0;
+        m_Checksum = 0;        
+        m_Temp = 0;
+        m_CurrentOperation = Operation_BeginRead;
     }
 
-    switch (_currentOperation)
+    switch (m_CurrentOperation)
     {
         case Operation_BeginRead:
             if (input == 0x02) {
-                _currentOperation = Operation_ReadMSB;
+                m_CurrentOperation = Operation_ReadMSB;
             }
             break;                    
         case Operation_ReadMSB:
-            _temp = ASCIIToByte(input);
-            _currentOperation = Operation_ReadLSB;
+            m_Temp = ASCIIToByte(input);
+            m_CurrentOperation = Operation_ReadLSB;
             break;
         case Operation_ReadLSB:
             input = ASCIIToByte(input);
-            _temp = (_temp << 4) | input;
-            _Payload[_cnt++] = _temp;
-            if (_cnt > 5) {
-                _currentOperation = Operation_EndRead;
+            m_Temp = (m_Temp << 4) | input;
+            m_Payload.m_Payload[m_Cnt++] = m_Temp;
+            if (m_Cnt > Payload::DATA_SIZE) {
+                m_CurrentOperation = Operation_EndRead;
             }
             else {
-                _currentOperation = Operation_ReadMSB;
-                _checksum ^= _temp;
+                m_CurrentOperation = Operation_ReadMSB;
+                m_Checksum ^= m_Temp;
             }
             break;
         case Operation_EndRead:
             if (input == 0x03) {
-                _currentOperation = Operation_ValidateChecksum;
+                m_CurrentOperation = Operation_ValidateChecksum;
             }
             else {
-                _currentOperation = Operation_BeginRead;
+                m_CurrentOperation = Operation_BeginRead;
             }
         case Operation_ValidateChecksum:
             break;
     }
 
-    if (_currentOperation == Operation_ValidateChecksum)
+    if (m_CurrentOperation == Operation_ValidateChecksum)
     {
-        _currentOperation = Operation_BeginRead;
-        _cnt = 0;
-        if (_checksum == _Payload[5]) {
+        m_CurrentOperation = Operation_BeginRead;
+        m_Cnt = 0;
+        if (m_Checksum == m_Payload.m_Checksum) {
             return true;
         }
     }
@@ -70,11 +98,15 @@ bool RDM6300::decode(uint8_t input) {
     return false;
 }
 
-uint8_t RDM6300::ASCIIToByte(uint8_t input)
+uint8_t ASCIIToByte(uint8_t input)
 {
+    // 0x30 - 0x39 : ASCII '0' - '9'
+    // 0x41 - 0x46 : ASCII 'A' - 'F'
+
     uint8_t val = input;
     val -= 0x30; //0x30 is '0' in the ASCII table
     if (val > 9)
         val -= 7; //There are 7 symbols between 9 and A in the ASCII table- namely : ; < = > ? @
     return val;
+}
 }
--- arduino/RDM6300.h
+++ arduino/RDM6300.h
@@ -1,35 +1,62 @@
-/*
-  RFID RDM6300 data decoder library
-  (c) Stephane Driussi 20150623
-  Not for commercial use
-*/
-
 #ifndef RDM6300_H
 #define RDM6300_H
-
-class RDM6300 {
-public:
-    bool decode(uint8_t input);
-    
-    RDM6300(uint8_t Data[]);
-    uint8_t *_Payload; // used for read comparisons
-private:
-    static uint8_t ASCIIToByte(uint8_t input);
-    enum Operation
+#include <initializer_list>
+#include <array>
+namespace RFID
+{
+    struct Payload
     {
-        Operation_BeginRead,
-        Operation_ReadMSB,
-        Operation_ReadLSB,
-        Operation_ValidateChecksum,
-        Operation_EndRead
+        static const unsigned PAYLOAD_SIZE = 6;
+        static const unsigned DATA_SIZE = 5;
+        static const unsigned ID_SIZE = 3;
+        static const unsigned MANUFACTURER_SIZE = 2;
+
+        Payload() = default;
+        Payload(const std::initializer_list<uint8_t>& list);
+
+        bool operator==(const Payload& other) const;
+        union
+        {
+            uint8_t m_Payload[PAYLOAD_SIZE];
+            struct
+            {
+                uint8_t m_Data[DATA_SIZE];
+                uint8_t m_Checksum;
+            };
+            struct
+            {
+                uint8_t m_Manufacturer[MANUFACTURER_SIZE];
+                uint8_t m_ID[ID_SIZE];
+                uint8_t m_Checksum;
+            };
+        };
+        static_assert(PAYLOAD_SIZE - (ID_SIZE + MANUFACTURER_SIZE) == 1, "PAYLOAD_SIZE must be exactly 1 uint8_t bigger than ID_SIZE");
+        static_assert(DATA_SIZE == ID_SIZE + MANUFACTURER_SIZE, "DATA_SIZE must be exactly ID_SIZE + MANUFACTURER_SIZE");
     };
-private:
-    int _cnt;
-    Operation _currentOperation;
-    uint8_t _temp, _checksum;
-};
 
+    class RDM6300
+    {
+    public:
+        bool decode(uint8_t input);
+        RDM6300(Payload& p);
+    private:
+
+        enum Operation : uint8_t
+        {
+            Operation_BeginRead,
+            Operation_ReadMSB,
+            Operation_ReadLSB,
+            Operation_ValidateChecksum,
+            Operation_EndRead
+        };
+    private:
+        uint8_t m_Cnt;
+        Operation m_CurrentOperation;
+        uint8_t m_Temp, m_Checksum;
+        Payload& m_Payload;
+    };
+
+    static uint8_t ASCIIToByte(uint8_t input);
+}
 #endif
-
-
 
